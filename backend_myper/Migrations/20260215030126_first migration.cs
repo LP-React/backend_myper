d671ea0ca036b747bd4a2f333f88bba7b6c865af
LP-React <ljamirp30@gmail.com>
2026-02-14 22:06:30 -0500

    feat(db): first migration with EF Core

---
+++ backend_myper/Migrations/20260215030126_first migration.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace backend_myper.Migrations
+{
+    /// <inheritdoc />
+    public partial class firstmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TipoDocumento",
+                columns: table => new
+                {
+                    TipoDocumentoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TipoDocumento", x => x.TipoDocumentoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Trabajador",
+                columns: table => new
+                {
+                    TrabajadorId = table.Column<int>(type: "int", nullable: false),
+                    Nombres = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    Apellidos = table.Column<string>(type: "nvarchar(80)", maxLength: 80, nullable: false),
+                    NumeroDocumento = table.Column<int>(type: "int", nullable: false),
+                    FechaNacimiento = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FotoUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Direccion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Estado = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trabajador", x => x.TrabajadorId);
+                    table.ForeignKey(
+                        name: "FK_Trabajador_TipoDocumento_TrabajadorId",
+                        column: x => x.TrabajadorId,
+                        principalTable: "TipoDocumento",
+                        principalColumn: "TipoDocumentoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TipoDocumento",
+                columns: new[] { "TipoDocumentoId", "Nombre" },
+                values: new object[,]
+                {
+                    { 1, "DNI" },
+                    { 2, "Pasaporte" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Trabajador");
+
+            migrationBuilder.DropTable(
+                name: "TipoDocumento");
+        }
+    }
+}
