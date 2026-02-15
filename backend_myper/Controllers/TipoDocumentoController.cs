ce9c1c161af98c80fca5f49d3fdc0b5a36de450d
LP-React <ljamirp30@gmail.com>
2026-02-15 00:15:37 -0500

    feat(api): add TipoDocumentoDTO and TipoDocumentoController

---
+++ backend_myper/Controllers/TipoDocumentoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using Microsoft.EntityFrameworkCore;
+using backend_myper.Entities;
+using backend_myper.Context;
+using backend_myper.DTOs;
+
+namespace backend_myper.Controllers
+{
+    [Route("api/tipo-documentos")]
+    [ApiController]
+    public class TipoDocumentoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public TipoDocumentoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<TipoDocumentoDTO>>> Get()
+        {
+            var listaDTO = new List<TipoDocumentoDTO>();
+
+            foreach(var item in await _context.TipoDocumento.ToListAsync()){
+                listaDTO.Add(new TipoDocumentoDTO
+                {
+                    TipoDocumentoId = item.TipoDocumentoId,
+                    Nombre = item.Nombre
+                });
+            };
+
+            return Ok(listaDTO);
+        }
+    }
+}
---
+++ backend_myper/DTOs/TipoDocumentoDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace backend_myper.DTOs
+{
+    public class TipoDocumentoDTO
+    {
+        public int TipoDocumentoId { get; set; }
+        public string Nombre { get; set; }
+    }
+}
