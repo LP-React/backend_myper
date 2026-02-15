76a1faafcea3ef89a6728272353bf709d7317c1d
Laysson Polo <109695735+LP-React@users.noreply.github.com>
2026-02-15 12:07:36 -0500

    refactor(db): change NumeroDocumento type to string in Trabajador entity (#6)

--- backend_myper/Migrations/20260215165432_ChangeNumeroDocumentoToString.Designer.cs
+++ backend_myper/Migrations/20260215165432_ChangeNumeroDocumentoToString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using backend_myper.Context;
 
@@ -11,9 +12,11 @@ using backend_myper.Context;
 namespace backend_myper.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260215165432_ChangeNumeroDocumentoToString")]
+    partial class ChangeNumeroDocumentoToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
