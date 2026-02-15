4c1e239fb443d0d553a0e624d75561f20af01dbc
LP-React <ljamirp30@gmail.com>
2026-02-14 20:51:50 -0500

    feat(entities): add Trabajador and TipoDocumento entities

--- backend_myper/Entities/Trabajador.cs
+++ backend_myper/Entities/Trabajador.cs
@@ -2,5 +2,15 @@
 {
     public class Trabajador
     {
+        public int TrabajadorId { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
+        public int NumeroDocumento { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Sexo { get; set; }
+        public string FotoUrl { get; set; }
+        public string Direccion { get; set; }
+        public bool Estado { get; set; }
     }
 }
